24a13d197bf41175f098bcaf422a91d8564c4c4c
Hannu Packalen <hpackalen@lcs.on.ca>
2025-01-27 09:07:59 -0500

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Hannu Packalen on 2025-01-27.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
